26adaefad802dae44df72e6e44fb8ff5968ffcfc
Irtizahussain10 <irtizahussain221@gmail.com>
2021-07-21 21:28:33 -0700

    Added more css functionality

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -2,15 +2,25 @@ import React from "react";
 import "./home.css";
 
 class Home extends React.Component {
+  url1 =
+    "https://www.listchallenges.com/f/lists/3d06c742-15cc-49b2-8845-7ef42d0c9f97.jpg";
+  url2 =
+    "https://www.rd.com/wp-content/uploads/2018/12/shutterstock_1161597079.jpg";
   render() {
     return (
-      <div>
-        <h1>ABOUT US</h1>
-        <p>
-          We bring comfort and ease to your lives by connecting you with
-          countless restaurants spread all over Karachi. Book your reservations
-          or order now!
-        </p>
+      <div className="homePage">
+        <div className="aboutUs">
+          <h1>ABOUT US</h1>
+          <p>
+            We bring comfort and ease to your lives by connecting you with
+            countless restaurants spread all over Karachi. Book your
+            reservations or order now from the comforts of your home!
+          </p>
+        </div>
+        <div className="images">
+          <img src={this.url1} className="img1" />
+          <img src={this.url2} className="img2" />
+        </div>
       </div>
     );
   }
