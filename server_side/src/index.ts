22918b3dcc1872e4f8e0667c8bfa0b5bba76edb0
Irtizahussain10 <irtizahussain221@gmail.com>
2021-07-25 00:24:42 -0700

    connection with database made

--- server_side/src/index.ts
+++ server_side/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import cors from "cors";
 import helmet from "helmet";
@@ -17,6 +18,27 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@sandbox.ibylc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+async function connect() {
+  try {
+    const client = new MongoClient(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    let connection = await client.connect();
+    let query = await connection
+      .db("sample_restaurants")
+      .collection("restaurants")
+      .findOne();
+    console.log(query);
+    await client.close();
+  } catch (e) {
+    console.error(e);
+  }
+}
+
 app.listen(PORT, () => {
+  connect();
   console.log(`Listening at port ${PORT}`);
 });
