3269008cd529d35cd10403bf85e138b458e9feca
Irtizahussain10 <irtizahussain221@gmail.com>
2021-07-24 23:07:43 -0700

    express server with typescript created

---
+++ server_side/src/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+
+dotenv.config();
+
+if (!process.env.PORT) {
+  process.exit(1);
+}
+
+const PORT: number = parseInt(process.env.PORT as string, 10);
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Listening at port ${PORT}`);
+});
